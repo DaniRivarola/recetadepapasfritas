898c9766872203fe6354478dd85b19c5906cf0b0
DaniRivarola <104107467+DaniRivarola@users.noreply.github.com>
2022-04-22 20:10:03 -0300

    Initial commit

---
+++ src/ejercicio1.ts
@@ -0,0 +1,12 @@
+console.log("hacer papas fritas");
+console.log("seleccionar las papas");
+
+console.log("lavar las papas");
+console.log("pelar las papas");
+console.log("cortar las papas en rodajas o bastones");
+console.log("secar las papas");
+console.log("poner aceite en una sarten");
+console.log("prender la hornalla y calentar el aceite");
+console.log("poner las papas en el aceite caliente");
+console.log("dejar las papas en el aceite hasta que esten cocinadas y doradas");
+console.log("colocarlas en papel absorventes");
